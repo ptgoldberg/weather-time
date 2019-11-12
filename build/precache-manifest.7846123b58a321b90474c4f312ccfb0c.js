104b6b1677d0f3d351e6c18c72dfc3a33c7d7694
Paul <thepaulgoldberg@gmail.com>
2019-11-12 15:52:39 -0800

    Add files via upload

---
+++ build/precache-manifest.7846123b58a321b90474c4f312ccfb0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c456d60235fc5daecf169a2955c4b807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37bd5bea9be1fac33624",
+    "url": "/static/js/2.5142b28c.chunk.js"
+  },
+  {
+    "revision": "d253956588f0456b0d47",
+    "url": "/static/js/main.64ad101a.chunk.js"
+  },
+  {
+    "revision": "3e95bd9de01710d43a49",
+    "url": "/static/js/runtime-main.d84ea1a8.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,75 @@
+/*jshint esversion: 6 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Request from 'superagent';
+//import _ from 'lodash';
+
+
+class Input extends React.Component {
+
+    constructor() { 
+        super();
+        this.state = {
+            zip: 98119
+        };
+    }
+
+    componentWillMount () {
+        this.search();
+    }
+
+    updateSearch () {
+        this.search(this.refs.query.value);
+    }
+
+    clicked () {
+        this.setState({
+            zip: this.refs.query.value
+        });
+        this.search(this.refs.query.value);
+    }
+
+    render() {
+        if (!this.state.weather) {
+            return <div>
+            <input ref="query" type="text" />
+            <button onClick={ (e) => {this.clicked(); }}>Search</button>
+            </div>
+        }
+
+        var temp = this.state.weather.main.temp
+        temp = Math.floor(((temp - 273.15)*9/5 + 32))
+        console.log(this.state.weather);
+
+        return <div>
+            <h1>Y'all want to know the weather in {this.state.weather.name}?</h1>
+            <input ref="query" type="text" placeholder="zip code"/>
+            <button onClick={ (e) => {this.clicked(); }}>Search</button>
+            <h2>{ this.state.weather.name }</h2>
+            <h4>{ this.state.weather.weather[0].description }</h4>
+            <h4>temperature: { temp }° F</h4>
+            <h4>wind speed: { this.state.weather.wind.speed } mph</h4>
+            <h4>humidity: { this.state.weather.main.humidity }%</h4>
+
+            
+        </div>
+    }
+
+    search(query = "98119") {
+        var url = `http://api.openweathermap.org/data/2.5/weather?zip=${query}&appid=65036b8d41536ad067f7d3079698ebcc`;
+        var mapUrl = 'https://tile.openweathermap.org/map/precipitation_new/4/{x}/{y}.png?appid=65036b8d41536ad067f7d3079698ebcc';
+        Request.get(url).then((response) => {
+            this.setState({
+                weather: response.body
+            });
+        });
+        
+        
+    }
+}
+
+  ReactDOM.render(
+    <Input />,
+    document.getElementById('root')
+  );
