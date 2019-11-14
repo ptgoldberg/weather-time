578e8504f1e5a7ec08c80395c5207c26bb4f0cb7
PG Work <paugoldb@amazon.com>
2019-11-14 07:37:25 -0800

    11.14.19

--- src/index.js
+++ src/index.js
@@ -32,33 +32,31 @@ class Input extends React.Component {
 
     render() {
         if (!this.state.weather) {
-            return <div>
-            <input ref="query" type="text" />
-            <button onClick={ (e) => {this.clicked(); }}>Search</button>
-            </div>
-        }
-
+            return  <div>
+                        <h1>Y'all want to know the weather? Plug that zip code in brah</h1>
+                        <input ref="query" type="text" placeholder='zip code'/>
+                        <button onClick={ (e) => {this.clicked(); }}>Search</button>
+                    </div>
+        } else { 
         var temp = this.state.weather.main.temp
         temp = Math.floor(((temp - 273.15)*9/5 + 32))
         console.log(this.state.weather);
 
-        return <div>
-            <h1>Y'all want to know the weather in {this.state.weather.name}?</h1>
-            <input ref="query" type="text" placeholder="zip code"/>
-            <button onClick={ (e) => {this.clicked(); }}>Search</button>
-            <h2>{ this.state.weather.name }</h2>
-            <h4>{ this.state.weather.weather[0].description }</h4>
-            <h4>temperature: { temp }° F</h4>
-            <h4>wind speed: { this.state.weather.wind.speed } mph</h4>
-            <h4>humidity: { this.state.weather.main.humidity }%</h4>
-
-            
-        </div>
+        return  <div>
+                    <h1>Y'all want to know the weather in {this.state.weather.name}?</h1>
+                    <input ref="query" type="text" placeholder="zip code"/>
+                    <button onClick={ (e) => {this.clicked(); }}>Search</button>
+                    <h2>{ this.state.weather.name }</h2>
+                    <h4>{ this.state.weather.weather[0].description }</h4>
+                    <h4>temperature: { temp }° F</h4>
+                    <h4>wind speed: { this.state.weather.wind.speed } mph</h4>
+                    <h4>humidity: { this.state.weather.main.humidity }%</h4>
+                </div>
+        }
     }
 
-    search(query = "98119") {
+    search(query = "") {
         var url = `https://api.openweathermap.org/data/2.5/weather?zip=${query}&appid=65036b8d41536ad067f7d3079698ebcc`;
-        var mapUrl = 'https://tile.openweathermap.org/map/precipitation_new/4/{x}/{y}.png?appid=65036b8d41536ad067f7d3079698ebcc';
         Request.get(url).then((response) => {
             this.setState({
                 weather: response.body
